23695f029d9492a2d2c14df50560522fea9ee1de
Chanchai00 <chanchaipiapeung@gmail.com>
2025-10-21 10:00:25 +0700

    add ReportLostTime

--- src/repositories/reportall.repository.js
+++ src/repositories/reportall.repository.js
@@ -51,4 +51,11 @@ exports.delete = async (id) =>
             type: db.sequelize.QueryTypes.SELECT,
           }
         );
-    
+        
+  exports.report_lost_time = async (sql) =>
+  await db.sequelize.query(
+    `${sql}`,
+    {
+     type: db.sequelize.QueryTypes.SELECT,
+    }
+  );  
